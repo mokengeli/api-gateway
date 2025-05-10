413e208674af5a907dc741aaf58f321bf5565e84
emmanuel <emmanuel.babala@dame-prevoyance.com>
2025-05-10 14:08:36 +0200

    feat: add cors

--- src/main/java/com/bacos/mokengeli/biloko/config/CorsGlobalConfiguration.java
+++ src/main/java/com/bacos/mokengeli/biloko/config/CorsGlobalConfiguration.java
@@ -1,19 +1,32 @@
 package com.bacos.mokengeli.biloko.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsGlobalConfiguration {
+    private final String allowedOrigins;
+
+    @Autowired
+    public CorsGlobalConfiguration(@Value("${security.cors.allowed-origins}") String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         // Autoriser les demandes en provenance de http://localhost:3000
-        corsConfig.addAllowedOrigin("http://localhost:3000");
+        corsConfig.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         // Autoriser toutes les méthodes (GET, POST, PUT, DELETE, OPTIONS, ...)
         corsConfig.addAllowedMethod("*");
         // Autoriser tous les headers ou bien ceux dont vous avez besoin
